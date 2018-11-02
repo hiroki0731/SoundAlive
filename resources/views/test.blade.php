cf095c474e6fe80f2c0568701252a0a879ce67e7
h.takada <h.takada0731@gmail.com>
2018-11-02 15:28:34 +0900

    テストコミット

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>気軽なライブ検索サイト</title>
+</head>
+<body>
+    <header>ヘッダー</header>
+    中身
+    <footer>フッター</footer>
+</body>
+</html>
