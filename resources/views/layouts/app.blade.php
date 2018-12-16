a53e781106507511c19a678bad1ecfb66e8a6b09
h.takada <h.takada0731@gmail.com>
2018-12-16 18:23:50 +0900

    FBの使用が変わって、どうやっても自動投稿できなさそうだからやめた

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,9 @@
                     <a href="https://twitter.com/SoundAlive2">
                         <i class="fab fa-twitter-square fa-3x footer-icon"></i>
                     </a>
-                    <a href="https://www.facebook.com/SoundAlive-%E3%83%90%E3%83%B3%E3%83%89%E7%B7%8F%E5%90%88%E6%A4%9C%E7%B4%A2%E3%82%B5%E3%82%A4%E3%83%88-2181257818792730">
-                        <i class="fab fa-facebook-square fa-3x footer-icon"></i>
-                    </a>
+                    {{--<a href="https://www.facebook.com/SoundAlive-%E3%83%90%E3%83%B3%E3%83%89%E7%B7%8F%E5%90%88%E6%A4%9C%E7%B4%A2%E3%82%B5%E3%82%A4%E3%83%88-2181257818792730">--}}
+                        {{--<i class="fab fa-facebook-square fa-3x footer-icon"></i>--}}
+                    {{--</a>--}}
                     <p>SoundAlive公式</p>
                     <p>Copyright © Freedom Freak All Rights Reserved.</p>
                 </div>
