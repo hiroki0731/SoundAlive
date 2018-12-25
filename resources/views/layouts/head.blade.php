68b63198f1e19d40c3a9873104aca14c9077bfac
h.takada <h.takada0731@gmail.com>
2018-12-25 23:10:51 +0900

    normalize.css導入してみた

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -26,6 +26,7 @@
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/normalize.css') }}" rel="stylesheet">
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 <link href="{{ asset('css/mypage.css') }}" rel="stylesheet">
 <link href="{{ asset('css/search.css') }}" rel="stylesheet">
